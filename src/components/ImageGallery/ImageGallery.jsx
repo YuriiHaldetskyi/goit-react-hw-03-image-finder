866972dcde440d81a9432a8bba86b4a1d99af31c
YuriiHaldetskyi <uragaldetskiy@ukr.net>
2023-03-07 12:52:27 +0200

    fixed

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -6,7 +6,7 @@ import { Button } from 'components/Button/Button';
 
 export class ImageGallery extends Component {
   state = {
-    topic: null,
+    topic: [],
     error: null,
     status: 'idle',
     pages: 12,
@@ -23,40 +23,39 @@ export class ImageGallery extends Component {
 
   getItems = () => {
     const KEY = `33196555-32542256d6492fa532620aad6`;
-    {
-      this.setState({ status: 'pending' });
-      fetch(
-        `https://pixabay.com/api/?q=${this.props.topic}&page=1}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${this.state.pages}`
-      )
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          }
+    this.setState({ status: 'pending' });
+    fetch(
+      `https://pixabay.com/api/?q=${this.props.topic}&page=1}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${this.state.pages}`
+    )
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
 
-          return Promise.reject(
-            new Error(
-              `Sorry we dont have a picture witch name is ${this.props.topic}`
-            )
-          );
-        })
-        .then(topic => {
-          if (topic.hits.length !== 0) {
-            this.setState({ topic, status: 'resolved' });
-          } else {
-            this.setState({
-              error: `Sorry topic ${this.props.topic} is absent`,
-              status: 'rejected',
-            });
-          }
-        })
-        .catch(error => this.setState({ error, status: 'rejected' }));
-    }
+        return Promise.reject(
+          new Error(
+            `Sorry we dont have a picture witch name is ${this.props.topic}`
+          )
+        );
+      })
+      .then(topic => {
+        if (topic.hits.length !== 0) {
+          this.setState({ topic, status: 'resolved' });
+        } else {
+          this.setState({
+            error: `Sorry topic ${this.props.topic} is absent`,
+            status: 'rejected',
+          });
+        }
+      })
+      .catch(error => this.setState({ error, status: 'rejected' }));
   };
   toggleModal = () =>
     this.setState(prevState => ({ showModal: !prevState.showModal }));
 
   loadMore = () =>
     this.setState(prevState => ({ pages: prevState.pages + 12 }));
+
   render() {
     const { status, topic, error } = this.state;
     if (status === 'idle') {
@@ -82,5 +81,3 @@ export class ImageGallery extends Component {
     }
   }
 }
-
-// key 33196555-32542256d6492fa532620aad6
